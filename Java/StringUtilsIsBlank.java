c5801c56f0b23ced1cb7991615b6955086a71372
Rob Tompkins <chtompki@gmail.com>
2018-10-22 09:23:21 -0400

    (add) java StringUtilsIsBlank.isBlank(String str)

---
+++ Java/StringUtilsIsBlank.java
@@ -0,0 +1,22 @@
+/**
+ * A class for determining if a <code>String</code> is either <code>null</code> or <code>""</code>.
+ *
+ * @author Rob Tompkins
+ */
+public static class StringUtilsIsBlank {
+
+    private StringUtils() {
+        //Hide utility class constructor
+    }
+
+    /**
+     * Returns <code>true</code> if a <code>String</code> is either <code>null</code> or if the <code>String</code>
+     * is <code>""</code>.
+     * @param str is the <code>String</code> to check.
+     * @return <code>true</code> if the <code>String</code>.
+     */
+    public static boolean isBlank(String str) {
+        return str == null || "".equals(str);
+    }
+
+}
